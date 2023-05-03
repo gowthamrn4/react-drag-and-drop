94b7cae5752eb9b11f97205d546ff972ea945854
gowthamrn4 <40823819+gowthamrn4@users.noreply.github.com>
2023-05-03 14:05:38 +0100

    drag and drop event api completd

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ export default function App() {
     },
   ]);
 
-  const handleChange = (item, status) => {
+  const handleChange = (id, status) => {
     const state = data.map((value) =>
-      value.id == item.id
+      value.id == id
         ? {
             ...value,
             status: status,
@@ -23,8 +23,18 @@ export default function App() {
     setData(state);
   };
 
-  const onDragStart = (e,item) => {
+  const onDragStart = (e, item) => {
     e.dataTransfer.setData('dataId', item.id);
+  };
+
+  const dropItem = (e, status) => {
+    e.preventDefault();
+    var id = e.dataTransfer.getData('dataId');
+    handleChange(id, status);
+  };
+
+  const allowDrop = (e) => {
+    e.preventDefault();
   };
 
   const renderData = (currentStatus, nextStatus) => {
@@ -36,7 +46,7 @@ export default function App() {
           className="item-box"
           key={index}
           draggable="true"
-          onDragStart={(e) => onDragStart(e,item)}
+          onDragStart={(e) => onDragStart(e, item)}
         >
           {item.title}
         </div>
@@ -45,9 +55,27 @@ export default function App() {
   return (
     <>
       <div className="container">
-        <div className="box">{renderData('pending', 'inprogress')}</div>
-        <div className="box">{renderData('inprogress', 'done')}</div>
-        <div className="box">{renderData('done', 'pending')}</div>
+        <div
+          className="box"
+          onDrop={(e) => dropItem(e, 'pending')}
+          onDragOver={(e) => allowDrop(e)}
+        >
+          {renderData('pending', 'inprogress')}
+        </div>
+        <div
+          className="box"
+          onDrop={(e) => dropItem(e, 'inprogress')}
+          onDragOver={(e) => allowDrop(e)}
+        >
+          {renderData('inprogress', 'done')}
+        </div>
+        <div
+          className="box"
+          onDrop={(e) => dropItem(e, 'done')}
+          onDragOver={(e) => allowDrop(e)}
+        >
+          {renderData('done', 'pending')}
+        </div>
       </div>
       <div className="input-container"></div>
     </>
